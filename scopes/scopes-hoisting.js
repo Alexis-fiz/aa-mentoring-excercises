f53bee4d7ee441133ce7178683f8c62358971300
Alexis_fiz <alexis.hennaut@gmail.com>
2023-04-06 19:47:30 +0300

    add scope and hoisting excercises draft

---
+++ scopes/scopes-hoisting.js
@@ -0,0 +1,90 @@
+// if (true) {
+//   var villain = 'Joker'; 
+//   const hero = 'Batman';
+//   let sidekick = 'Robin';
+// }
+
+// console.log(villain); // Joker
+// console.log(hero); // ReferenceError: num is not defined
+// console.log(sidekick); // ReferenceError: num2 is not defined
+
+'---------------------------------------------------------';
+
+
+// console.log(hero); // ReferenceError: num is not defined
+// console.log(sidekick); // ReferenceError: num2 is not defined
+
+// const hero = 10;
+// let sidekick = 5;
+
+'---------------------------------------------------------';
+
+// const hero = 'Batman';
+// hero = 'Spiderman';
+
+// console.log(hero); // TypeError: Assignment to constant variable.
+
+'---------------------------------------------------------';
+
+
+
+// console.log(villain); // undefined
+// myQuote(); // I'm Batman!
+
+// var villain = 'Joker'; 
+
+// function myQuote() {
+//   console.log('I'm Batman!');
+// }
+
+'---------------------------------------------------------';
+
+// console.log(sidekick); // ReferenceError: num is not defined
+// myQuote(); // ReferenceError: myFunc is not defined
+
+// let sidekick = 'Robin';
+
+// function myQuote() {
+//   console.log('I'm Batman!');
+// }
+
+'---------------------------------------------------------';
+
+// if (true) {
+//   console.log(villain); // undefined
+//   console.log(myQuote); // [Function: myQuote]
+
+//  var villain = 'Joker'; 
+
+//  function myQuote() {
+//    console.log('I'm Batman!');
+//  }
+// }
+
+// console.log(villain); // 'Joker'
+// console.log(myQuote); // [Function: myQuote]
+
+'---------------------------------------------------------';
+
+// var a = 1;
+// function outer() {
+//   console.log(a);
+//   function inner() {
+//     console.log(a);
+//     var a = 2;
+//   }
+//   inner();
+// }
+// outer();
+
+
+// function outer() {
+//   var a;
+//   console.log(a);
+//   function inner() {
+//     var a;
+//     console.log(a);
+//     a = 2;
+//   }
+//   inner();
+// }
