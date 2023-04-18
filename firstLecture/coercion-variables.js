d00effc8f61ca74989e4ac21db77b68703c4857b
Alexis_fiz <alexis.hennaut@gmail.com>
2023-04-19 00:39:20 +0300

    adds excercises for the first lecture

---
+++ firstLecture/coercion-variables.js
@@ -0,0 +1,99 @@
+// What is the result of the following code?
+// console.log("5" + true);
+// a) "5true"
+// b) 6
+// c) "6"
+// d) TypeError
+
+// "5true"
+
+// console.log("" || "hello");
+// a) ""
+// b) "hello"
+// c) null
+// d) undefined
+
+// "hello"
+
+// console.log("0" == false);
+// a) true
+// b) false
+// c) TypeError
+// d) ReferenceError
+
+// true
+
+// console.log(10 == "10");
+// a) true
+// b) false
+// c) TypeError
+// d) ReferenceError
+
+// true
+
+// console.log("5" - 2);
+// a) 3
+// b) "3"
+// c) 7
+// d) "7"
+
+// 3
+
+// console.log("5" + 3);
+// a) "8"
+// b) 8
+// c) "53"
+// d) 53
+
+// 53
+
+// What is the result of the following code?
+// let a = 5;
+// let b = a;
+// b = 10;
+// console.log(a);
+
+// a) 5
+// b) 10
+// c) TypeError
+// d) ReferenceError
+
+// 5
+
+// What is the result of the following code?
+// let a = [1, 2, 3];
+// let b = a;
+// b.push(4);
+// console.log(a);
+
+// a) [1, 2, 3]
+// b) [1, 2, 3, 4]
+// c) [4]
+// d) TypeError
+
+// [1, 2, 3, 4]
+
+// What is the result of the following code?
+// let a = {name: "John"};
+// let b = a;
+// b.name = "Sarah";
+// console.log(a.name);
+// a) "John"
+// b) "Sarah"
+// c) TypeError
+// d) ReferenceError
+
+// "Sarah"
+
+
+// What is the difference between let and var in JavaScript?
+// a) let variables have block scope, while var variables have function scope.
+// b) let variables have function scope, while var variables have block scope.
+// c) Both let and var variables have function scope.
+// d) Both let and var variables have block scope.
+
+// What is the difference between == and === operators in JavaScript?
+// a) == compares values and types, while === compares only values.
+// b) == compares only values, while === compares values and types.
+// c) == is the assignment operator, while === is the equality operator.
+// d) == is the equality operator, while === is the strict equality operator.
