bb4a23ff15bdd5478613e299977d6fbe8949d88a
Antonis Margaronis <antonis.margaronis@holidayextras.com>
2024-02-26 13:44:25 +0200

    Create final version

--- index.js
+++ index.js
@@ -1,74 +1,110 @@
 const TASKS = {
     spaceStone: {
-        element: document.getElementById("space-stone"),
-        divId: "space-stone",
-        eventType: "click",
+      element: document.getElementById('space-stone'),
+      divId: 'space-stone',
     },
     mindStone: {
-        element: document.getElementById("mind-stone"),
-        divId: "mind-stone",
-        eventType: "click",
+      element: document.getElementById('mind-stone'),
+      divId: 'mind-stone',
     },
     realityStone: {
-        element: document.getElementById("reality-stone"),
-        divId: "reality-stone",
-        eventType: "mouseover",
+      element: document.getElementById('reality-stone'),
+      divId: 'reality-stone',
     },
     powerStone: {
-        element: document.getElementById("power-stone"),
-        divId: "power-stone",
-        eventType: "click",
+      element: document.getElementById('power-stone'),
+      divId: 'power-stone',
     },
     timeStone: {
-        element: document.getElementById("time-stone"),
-        divId: "time-stone",
-        eventType: "click",
+      element: document.getElementById('time-stone'),
+      divId: 'time-stone',
     },
     soulStone: {
-        element: document.getElementById("soul-stone"),
-        divId: "soul-stone",
-        eventType: "click",
+      element: document.getElementById('soul-stone'),
+      divId: 'soul-stone',
     },
-}
-
-
-function getCompletedTasksFromStorage() {
-    return JSON.parse(localStorage.getItem('completedTasks')) || []
-}
-
-function setTaskCompletedInStorage(task) {
-    localStorage.setItem('completedTasks', JSON.stringify([...getCompletedTasksFromStorage(), task]))
-}
-
-function areAllTasksCompleted() {
-    return Object.keys(TASKS).every((task => getCompletedTasksFromStorage().includes(task)))
-}
-
-function markTaskAsCompleted(task) {
-    const div = document.getElementById(TASKS[task].divId)
-    if(!div) return
-    div.classList.add('infinity-stone-completed')
-}
-
-function onCompleteTask(task) {
-    const tasksOnStorage = getCompletedTasksFromStorage()
-    if(tasksOnStorage.includes(task)) return
-    setTaskCompletedInStorage(task)
-    markTaskAsCompleted(task)
-    if(areAllTasksCompleted()) window.location.href = "form.html"
-}
-
-function init() {
-    getCompletedTasksFromStorage().forEach(markTaskAsCompleted)
-
-    Object.keys(TASKS).forEach(task => {
-        const { element, eventType } = TASKS[task]
-        if(!element) return
-
-        element.addEventListener(eventType, () => {
-            onCompleteTask(task)
-        },{ once: true})
-    })
-}
-
-init()
+  };
+  
+  function getCompletedTasksFromStorage() {
+    // TODO
+    return JSON.parse(localStorage.getItem('completedTasks')) || [];
+  }
+  
+  function setTaskCompletedInStorage(task) {
+    // TODO
+    localStorage.setItem(
+      'completedTasks',
+      JSON.stringify([...getCompletedTasksFromStorage(), task])
+    );
+  }
+  
+  function markTaskAsCompleted(task) {
+    const div = document.getElementById(TASKS[task].divId);
+    if (!div) return;
+    div.classList.add('infinity-stone-completed');
+    // TODO: add the class infinity-stone-completed
+  }
+  
+  function onCompleteTask(task) {
+    setTaskCompletedInStorage(task);
+    markTaskAsCompleted(task);
+    onAllTasksCompleted();
+  }
+  
+  function onAllTasksCompleted() {
+    const areAllTasksCompleted = Object.keys(TASKS).every((task) =>
+      getCompletedTasksFromStorage().includes(task)
+    );
+    if (areAllTasksCompleted) window.location.href = 'form.html';
+  }
+  
+  function createEventListeners() {
+    // TODO
+    const options = { once: true };
+    const completedTasks = getCompletedTasksFromStorage();
+    if (!completedTasks.includes('spaceStone')) {
+      window.addEventListener(
+        'keydown',
+        (e) => {
+          if (e.key === ' ') onCompleteTask('spaceStone');
+        },
+        options
+      );
+    }
+    if (!completedTasks.includes('mindStone')) {
+      TASKS.mindStone.element.addEventListener(
+        'click',
+        () => onCompleteTask('mindStone'),
+        options
+      );
+    }
+    if (!completedTasks.includes('realityStone')) {
+      TASKS.realityStone.element.addEventListener(
+        'mouseover',
+        () => onCompleteTask('realityStone'),
+        options
+      );
+    }
+    if (!completedTasks.includes('powerStone')) {
+      window.addEventListener(
+        'resize',
+        () => onCompleteTask('powerStone'),
+        options
+      );
+    }
+    if (!completedTasks.includes('timeStone')) {
+      window.addEventListener('load', () => onCompleteTask('timeStone'), options);
+    }
+    if (!completedTasks.includes('soulStone')) {
+      TASKS.soulStone.element.addEventListener(
+        'click',
+        () => onCompleteTask('soulStone'),
+        options
+      );
+    }
+  }
+  
+  getCompletedTasksFromStorage().forEach(markTaskAsCompleted);
+  onAllTasksCompleted();
+  createEventListeners();
+  
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 displayAvengers();
 
-const username = document.getElementById("username");
-const password = document.getElementById("password");
+const username = document.getElementById('username');
+const password = document.getElementById('password');
 
 function validUsername() {
   const valueLowerCase = username.value.toLowerCase();
   if (valueLowerCase !== 'thanos' && valueLowerCase !== 'avengers') {
-    document.getElementById("username-error").textContent = "Username should be either thanos or avengers";
+    document.getElementById('username-error').textContent =
+      'Username should be either thanos or avengers';
     return false;
   }
   return true;
@@ -14,103 +15,134 @@ function validUsername() {
 
 function validPassowrd() {
   if (password.value !== username.value) {
-    document.getElementById("password-error").textContent = "Password must match Username";
+    document.getElementById('password-error').textContent =
+      'Password must match Username';
     return false;
   }
   return true;
 }
 
-username.addEventListener('focus', function() {
-  document.getElementById("username-error").textContent = "";
+// TODO: dinoume to callback do the rest
+username.addEventListener('focus', function () {
+  document.getElementById('username-error').textContent = '';
 });
 
-username.addEventListener('blur', function() {
+username.addEventListener('blur', function () {
   validUsername();
 });
 
-password.addEventListener('focus', function() {
-  document.getElementById("password-error").textContent = "";
+password.addEventListener('focus', function () {
+  document.getElementById('password-error').textContent = '';
 });
 
-password.addEventListener('blur', function() {
+password.addEventListener('blur', function () {
   validPassowrd();
 });
 
-document.getElementById("myForm").addEventListener("submit", async function(event) {
-  const isFormValid = validUsername() && validPassowrd();
-  event.preventDefault();
-  if (!isFormValid) {
-    return;
-  }
-
-  document.querySelector('.formBox').classList.add('hide');
-  
-  const response = await fetch('http://localhost:3000/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username: username.value, password: password.value }),
-    credentials: 'include' // Include cookies in the request and response
-  })
-  
-  const data = await response.json();
-  if (data?.role === 'Thanos') {
-    
-    // this is to be written by users
-    var thanosButton = document.createElement('button');
-    thanosButton.id = 'thanosButton'; 
-    thanosButton.textContent = 'Snap';
-    thanosButton.classList.add('btn')
-    document.querySelector('.button-container').appendChild(thanosButton);
-    
-    
-    thanosButton.addEventListener('click', async function() {
-      await getAvengersAlive();
-      displayAvengers();
-    }, { once: true })
-
-  } else if (data?.role === 'Avengers') {
-    
-    const avengersButton = document.createElement('button');
-    avengersButton.id = 'avengersButton'; 
-    avengersButton.textContent = 'Save';
-    avengersButton.classList.add('btn')
-    document.querySelector('.button-container').appendChild(avengersButton);
-    
-    
-    avengersButton.addEventListener('click', async function() {
-      await getAvengersAlive();
-      displayAvengers();
-    }, { once: true })
-  }
-});
-
-
-async function getAvengersAlive() {
-  const response = await fetch('http://localhost:3000/whosAvenger', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include'
-  })
-  const {avengers} = await response.json()
-  localStorage.setItem('avengers', JSON.stringify(avengers));
+function createThanosButton() {
+  var thanosButton = document.createElement('button');
+  thanosButton.id = 'thanosButton';
+  thanosButton.textContent = 'Snap';
+  thanosButton.classList.add('btn');
+  document.querySelector('.button-container').appendChild(thanosButton);
 }
 
+function createAvengersButton() {
+  const avengersButton = document.createElement('button');
+  avengersButton.id = 'avengersButton';
+  avengersButton.textContent = 'Save';
+  avengersButton.classList.add('btn');
+  document.querySelector('.button-container').appendChild(avengersButton);
+}
+
+async function onSnap(role) {
+  const aliveAvengers = await getAvengersAlive(role);
+  console.log(aliveAvengers, role);
+  localStorage.setItem('avengers', JSON.stringify(aliveAvengers));
+  displayAvengers();
+}
+
+document
+  .getElementById('myForm')
+  .addEventListener('submit', async function (event) {
+    const isFormValid = validUsername() && validPassowrd();
+    event.preventDefault();
+    if (!isFormValid) {
+      return;
+    }
+
+    document.querySelector('.formBox').classList.add('hide');
+    // const response = await fetch('http://localhost:3000/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     username: username.value,
+    //     password: password.value,
+    //   }),
+    // });
+
+    // const data = await response.json();
+    const data = { role: username.value };
+    if (data?.role === 'thanos') {
+      createThanosButton();
+      // this is to be written by users
+      document
+        .getElementById('thanosButton')
+        .addEventListener('click', () => onSnap('thanos'), { once: true });
+    } else if (data?.role === 'avengers') {
+      createAvengersButton();
+      document
+        .getElementById('avengersButton')
+        .addEventListener('click', () => onSnap('avengers'), { once: true });
+    }
+  });
+
+async function getAvengersAlive(role) {
+  // const response = await fetch(
+  //   'https://dtu-mentoring-server.onrender.com/whosAvenger',
+  //   {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     cors: 'omit',
+  //   }
+  // );
+  // const { avengers } = await response.json();
+  const avengers = [
+    'Iron Man',
+    'Captain America',
+    'Thor',
+    'Hulk',
+    'Black Widow',
+    'Hawkeye',
+    'Scarlet Witch',
+    'Vision',
+    'Black Panther',
+    'Spider-Man',
+  ];
+  if (role === 'thanos') return avengers.slice(0, 5);
+  return avengers;
+}
 
 function displayAvengers() {
+  // TODO Intro + HTML container D<div class="card">
+  // <p class="avenger-text">${avenger}</p>
+  // </div>`;
   const avengersString = localStorage.getItem('avengers');
-  if(!avengersString) return;
+  if (!avengersString) return;
 
   const avengers = JSON.parse(avengersString);
   const container = document.getElementById('avengersContainer');
-  avengers.forEach(avenger => {
+  container.innerHTML = '';
+
+  avengers.forEach((avenger) => {
     const cardHtml = `
     <div class="card">
         <p class="avenger-text">${avenger}</p>
     </div>`;
-    container.insertAdjacentHTML('beforeend', cardHtml); 
-  })
+    container.insertAdjacentHTML('beforeend', cardHtml);
+  });
 }
