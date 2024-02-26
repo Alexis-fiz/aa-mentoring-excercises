ff03913934fe6a1b71c9eb64a3d57e0c9fc92fcb
Antonis Margaronis <antonis.margaronis@holidayextras.com>
2024-02-26 15:56:29 +0200

    Add readme and organise main.js

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# aa-mentoring-excercises
+As we mentioned, Avengers infinity war is practically a story of events, cookies and local storage. So it's your time to shine. Marvel was very kind and provided us with the `<script />` of the movie. Sadly, some pieces got erased and we need your help to restore it. Complete the following tasks and watch the story unveil before your eyes!
+
+# Task 1: Collect all infinity stones
+The story begins with the goal of collecting all the infinity stones. In the UI you can find 6 buttons, each one for each stone. Time to start acting, open up `index.js`.
+
+Each stone needs a unique task to be performed, in order to collect it.
+- The space stone needs the user to hit the spacebar once
+- The mind stone needs the user to click one it (since some minds just click...)
+- The reality stone needs the window to resize
+- The power stone needs the cursor to pass on top of it
+- The time stone needs the window to load
+- The soul stone needs the user to really focus on it.
+
+Once a task has been completed run the `onCompleteTask` function with the task name as an argument. Additionally, change the `markTaskAsCompleted` function to add the 'infinity-stone-completed' class on the completed button.
+
+Finally, since you can't lose your progress you need to amend the `getCompletedTasksFromStorage` and `setTaskCompletedInStorage` functions to store the completedTasks in the storage.
+
+# Task 2: It's snapping time
+Now that you have all the infinity stones, it's time to get into action. Pick your role:
+- username: thanos - password: thanos
+- username: avengers - password: avengers
+
+First of all, we need to make sure you can log in. On the form
+- Make sure when you focus on the username and password fields, that it gets empty
+- Make sure when you lose focus from the fields, that the valiUsername and validPassword functions run
+
+Important! A function needs to submit, so make sure that when the function gets submited, the `onSubmit` function gets called. Speaking of `onSubmit` you might need to some changes there as well. As you can see it creates a button for the user depending on their role... but the button is not listening to anything! Make sure that the button calls the `onSnap` function when clicked.
+
+The `onSnap` function is what truly matters: it can either cut the world in half, or restore it... well that's what it's supposed to be doing anyway... as right now it is not saving anything in the storage. So change the `saveAvengersOnStorage` function to store the avengers on the storage.
+
+Finally, we need to see what we created! That means finalising the `displayAvengers` function. We need this function to:
+- Read the avengers from the storage
+- For each avengers we want to create a div of the following structure: 
+   `<div class="card">`
+        `<p class="avenger-text">${avenger}</p>`
+    `</div>`
+- Finally, append these divs on the `avengersContainer` div
+
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-displayAvengers();
-
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
@@ -13,7 +11,7 @@ function validUsername() {
   return true;
 }
 
-function validPassowrd() {
+function validPassword() {
   if (password.value !== username.value) {
     document.getElementById('password-error').textContent =
       'Password must match Username';
@@ -21,23 +19,6 @@ function validPassowrd() {
   }
   return true;
 }
-
-// TODO: dinoume to callback do the rest
-username.addEventListener('focus', function () {
-  document.getElementById('username-error').textContent = '';
-});
-
-username.addEventListener('blur', function () {
-  validUsername();
-});
-
-password.addEventListener('focus', function () {
-  document.getElementById('password-error').textContent = '';
-});
-
-password.addEventListener('blur', function () {
-  validPassowrd();
-});
 
 function createThanosButton() {
   var thanosButton = document.createElement('button');
@@ -55,82 +36,77 @@ function createAvengersButton() {
   document.querySelector('.button-container').appendChild(avengersButton);
 }
 
-async function onSnap(role) {
-  const aliveAvengers = await getAvengersAlive(role);
-  console.log(aliveAvengers, role);
-  localStorage.setItem('avengers', JSON.stringify(aliveAvengers));
-  displayAvengers();
+async function login() {
+  const response = await fetch(
+    'https://dtu-mentoring-server.onrender.com/login',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
+      credentials: 'include',
+    }
+  );
+  return await response.json();
 }
 
-document
-  .getElementById('myForm')
-  .addEventListener('submit', async function (event) {
-    const isFormValid = validUsername() && validPassowrd();
-    event.preventDefault();
-    if (!isFormValid) {
-      return;
+async function getAvengersAlive() {
+  const response = await fetch(
+    'https://dtu-mentoring-server.onrender.com/whosAvenger',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
     }
-
-    document.querySelector('.formBox').classList.add('hide');
-    // const response = await fetch('http://localhost:3000/login', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     username: username.value,
-    //     password: password.value,
-    //   }),
-    // });
-
-    // const data = await response.json();
-    const data = { role: username.value };
-    if (data?.role === 'thanos') {
-      createThanosButton();
-      // this is to be written by users
-      document
-        .getElementById('thanosButton')
-        .addEventListener('click', () => onSnap('thanos'), { once: true });
-    } else if (data?.role === 'avengers') {
-      createAvengersButton();
-      document
-        .getElementById('avengersButton')
-        .addEventListener('click', () => onSnap('avengers'), { once: true });
-    }
-  });
-
-async function getAvengersAlive(role) {
-  // const response = await fetch(
-  //   'https://dtu-mentoring-server.onrender.com/whosAvenger',
-  //   {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     cors: 'omit',
-  //   }
-  // );
-  // const { avengers } = await response.json();
-  const avengers = [
-    'Iron Man',
-    'Captain America',
-    'Thor',
-    'Hulk',
-    'Black Widow',
-    'Hawkeye',
-    'Scarlet Witch',
-    'Vision',
-    'Black Panther',
-    'Spider-Man',
-  ];
-  if (role === 'thanos') return avengers.slice(0, 5);
+  );
+  const { avengers } = await response.json();
   return avengers;
 }
 
+// ====================================================================== //
+
+function saveAvengersOnStorage(avengers) {
+  localStorage.setItem('avengers', JSON.stringify(avengers));
+}
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const isFormValid = validUsername() && validPassword();
+  if (!isFormValid) {
+    return;
+  }
+  document.querySelector('.formBox').classList.add('hide');
+  const { role } = await login();
+
+  switch (role) {
+    case 'Thanos':
+      createThanosButton();
+      document
+        .getElementById('thanosButton')
+        .addEventListener('click', onSnap, { once: true });
+      break;
+    case 'Avengers':
+      createAvengersButton();
+      document
+      .getElementById('avengersButton')
+      .addEventListener('click', onSnap, { once: true });
+      break;
+  }
+}
+
+async function onSnap() {
+  const aliveAvengers = await getAvengersAlive();
+  saveAvengersOnStorage(aliveAvengers)
+  displayAvengers();
+}
+
 function displayAvengers() {
-  // TODO Intro + HTML container D<div class="card">
-  // <p class="avenger-text">${avenger}</p>
-  // </div>`;
   const avengersString = localStorage.getItem('avengers');
   if (!avengersString) return;
 
@@ -146,3 +122,22 @@ function displayAvengers() {
     container.insertAdjacentHTML('beforeend', cardHtml);
   });
 }
+
+username.addEventListener('focus', function () {
+  document.getElementById('username-error').textContent = '';
+});
+
+username.addEventListener('blur', validUsername)
+
+
+password.addEventListener('focus', function () {
+  document.getElementById('password-error').textContent = '';
+});
+
+password.addEventListener('blur', validPassword);
+
+document
+  .getElementById('myForm')
+  .addEventListener('submit', onSubmit)
+
+displayAvengers();
